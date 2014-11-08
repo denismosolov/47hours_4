9a9d112df751b6e80695b905dd1060fbfe43142b
Dmitry <dr.jan.itor1988@gmail.com>
2014-11-08 15:37:34 +0400

    Bootsrap and start init

---
+++ src/panel/app/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bert
+ * Date: 11/8/14
+ * Time: 3:09 PM
+ */
+
+class IndexController extends \Phalcon\Mvc\Controller
+{
+    public function indexAction()
+    {
+        echo "<h1>Falcon!</h1>";
+    }
+}
