3b207241eaf4aa7cb380253a03b463a655c0a374
Denis Mosolov <denismosolov@gmail.com>
2014-11-09 00:32:30 +0300

    * skeleton for Cabinet

---
+++ src/panel/app/controllers/CabinetController.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CabinetController
+ *
+ * @author denis
+ */
+class CabinetController extends \Phalcon\Mvc\Controller
+{
+    public function indexAction() {
+        
+    }
+}
