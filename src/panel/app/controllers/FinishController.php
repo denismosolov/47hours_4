cd028e487e12d6a88a370bf508931192581beec6
leonid <koms@softgrad.com>
2014-11-08 23:04:17 +0300

    narkoman III kak Kaaaarl

--- src/panel/app/controllers/FinishController.php
+++ src/panel/app/controllers/FinishController.php
@@ -9,52 +9,14 @@
 class FinishController extends \Phalcon\Mvc\Controller
 {
     public function IndexAction(){
-
-        //Create a DI
-        $di = new Phalcon\DI\FactoryDefault();
-
-        // Setup the database service
-        $di->set('db', function(){
-
-            switch ($_SERVER['HTTP_HOST']) {
-                case "47hours.local":
-                    $password="root";
-                    $db_name = "panel";
-                    break;
-                case "panel.local":
-                    $password="YNRhZmJUWUN";
-                    $db_name = "paneld_db";
-                    break;
-                case "djaga.net":
-                    $password="LK434AaGLSg{}nqw41qove39t13";
-                    $db_name = "panel";
-                default:
-                    $password="";
-                    break;
-            }
-
-            return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
-                'host' => 'localhost',
-                'dbanme' => $db_name,
-                'username' => 'root',
-                'password' => $password
-            ));
-        });
-
-        echo "234";
-        exit;
-
         $request = $this->request->get();
-        SurveyResult::addNewResult(
+
+        UserSurvies::addNewResult(
             $request['user_id'],
             $request['survey_id'],
             date("Y-m-d H:i:s"),
             date("Y-m-d H:i:s"),
             $request['is_passed']
         );
-
-        
-
-
     }
 }
