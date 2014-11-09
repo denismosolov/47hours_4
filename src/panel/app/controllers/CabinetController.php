559dfbe12721f2e6a51ae27e2aa8b4563d586992
Denis Mosolov <denismosolov@gmail.com>
2014-11-09 07:00:48 +0300

    * added my surveys and open surveys to cabinet

--- src/panel/app/controllers/CabinetController.php
+++ src/panel/app/controllers/CabinetController.php
@@ -14,6 +14,40 @@
 class CabinetController extends \Phalcon\Mvc\Controller
 {
     public function indexAction() {
+        // for debug
+        // @todo: remove
+        $this->session->set('user_id', '1');
+        // end debug
         
+        if (! $this->session->has('user_id')) {
+            // unauthoried access
+            throw new Exception('An error has occured #1');
+        }
+        
+        // @todo: check how many respondents asked, is_opened
+        // @todo refactoring!!
+        $InvitedUsers = InvitedUsers::find(array(
+            'user_id' => $this->session->get('user_id')
+        ));
+        $open_surveys = array();
+        $my_surveys = array();
+        if (count($InvitedUsers)) {
+            foreach ($InvitedUsers as $row) {
+                $sid = $row->getSurveyId();
+                $US = UsersSurveys::findFirst(array(
+                    'user_id' => $this->session->get('user_id'),
+                    'survey_id' => $sid
+                ));
+                if ($US) {
+                    $my_surveys[] = Surveys::findFirst($sid);
+                } else {
+                    $open_surveys[] = Surveys::findFirst($sid);
+                }
+            }
+        } else {
+            $this->view->open_survey_ids = array();
+        }
+        $this->view->setVar('open_surveys', $open_surveys);
+        $this->view->setVar('my_surveys', $my_surveys);
     }
 }
