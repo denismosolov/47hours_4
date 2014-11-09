1ce46508e2753c97c56eca1df3c11881db4d69af
Dmitry <dr.jan.itor1988@gmail.com>
2014-11-09 12:02:41 +0400

    Changed login page as main

--- src/panel/public/index.php
+++ src/panel/public/index.php
@@ -55,6 +55,13 @@ try {
         return $session;
     });
 
+    $di->set('router', function () {
+        $router = new Phalcon\Mvc\Router\Annotations(false);
+        $router->setDefaultController('login');
+        $router->setDefaultAction('index');
+        return $router;
+    });
+
     //Handle the request
     $application = new \Phalcon\Mvc\Application($di);
 
